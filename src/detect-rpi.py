fea378e614eff8a01cf867848ec94cda3e57c2be
wijdanmkh-insk <wijdaninsaniak@gmail.com>
2026-05-25 18:45:12 +0700

    Fixing directory

--- src/detect-rpi.py
+++ src/detect-rpi.py
@@ -24,7 +24,7 @@ Path(os.path.dirname(JSON)).mkdir(parents=True, exist_ok=True)
 REQ_TIME = 3.0
 
 # 🛠️ PERBAIKAN 1: Hapus .to("cpu") di sini, kita akan paksa device="cpu" langsung di fungsi inferensi
-model = YOLO("../model/rpi.onnx")  
+model = YOLO("/model/rpi.onnx")  
 class_names = model.names
 print("✅ Model loaded: model/rpi.onnx")
 print(f"📋 Class names: {class_names}")
