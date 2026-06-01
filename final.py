4551a39f7bd9100295d037f7e615330cbc034ce2
wijdanmkh-insk <wijdaninsaniak@gmail.com>
2026-06-01 11:47:12 +0700

    Adding headless autorun

--- final.py
+++ final.py
@@ -18,7 +18,8 @@ SERVO_COOLDOWN = 5.0
 # SERIAL TO ARDUINO
 # =========================
 # Sesuaikan port dengan OS yang aktif
-arduino = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+port = "usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
+arduino = serial.Serial(f'/dev/{port}', 115200, timeout=1)
 time.sleep(2)
 print("✅ Arduino connected")
 
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/pi/ZEUS-CV
+
+source venv/bin/activate
+
+python headless.py
